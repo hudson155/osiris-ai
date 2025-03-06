66c23f106cb05f8c06e53aeeba25c93bda062637
Jeff Hudson <jeff@highbeam.co>
2025-03-06 17:03:26 -0500

    Configure build system

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+  `kotlin-dsl`
+}
+
+repositories {
+  gradlePluginPortal()
+}
+
+dependencies {
+  // https://kotlinlang.org/docs/releases.html#release-details
+  implementation(kotlin("gradle-plugin", "2.0.21"))
+
+  // https://plugins.gradle.org/plugin/com.google.cloud.artifactregistry.gradle-plugin
+  implementation("com.google.cloud.artifactregistry", "artifactregistry-gradle-plugin", "2.2.3")
+
+  // https://plugins.gradle.org/plugin/io.gitlab.arturbosch.detekt
+  implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.23.7")
+}
+
+kotlin {
+  explicitApi()
+  compilerOptions {
+    allWarningsAsErrors = true
+  }
+}
