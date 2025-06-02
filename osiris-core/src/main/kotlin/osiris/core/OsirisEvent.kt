16b5fd312f3c4d35eb185bf46fb866909aa42596
Jeff Hudson <jeff@highbeam.co>
2025-06-02 14:52:08 -0600

    Response has out variance

--- osiris-core/src/main/kotlin/osiris/core/OsirisEvent.kt
+++ osiris-core/src/main/kotlin/osiris/core/OsirisEvent.kt
@@ -5,5 +5,5 @@ import dev.langchain4j.data.message.ChatMessage
 public sealed class OsirisEvent<out Response : Any> {
   public data class Message(val message: ChatMessage) : OsirisEvent<Nothing>()
 
-  public data class Response<Response : Any>(val response: Response?) : OsirisEvent<Response>()
+  public data class Response<out Response : Any>(val response: Response?) : OsirisEvent<Response>()
 }
