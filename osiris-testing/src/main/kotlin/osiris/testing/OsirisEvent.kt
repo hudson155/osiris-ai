09bf59e0fee57b5251dd604adfaea63239ebfb47
Jeff Hudson <jeff@highbeam.co>
2025-06-02 12:01:41 -0600

    More types

--- osiris-testing/src/main/kotlin/osiris/testing/OsirisEvent.kt
+++ osiris-testing/src/main/kotlin/osiris/testing/OsirisEvent.kt
@@ -3,8 +3,8 @@ package osiris.testing
 import dev.langchain4j.data.message.ChatMessage
 import osiris.core.OsirisEvent
 
-public val <Response : Any> List<OsirisEvent<Response>>.response: Response?
-  get() = filterIsInstance<OsirisEvent.Response<Response>>().single().response
+public val List<OsirisEvent>.response: String?
+  get() = filterIsInstance<OsirisEvent.Response>().single().response
 
-public val List<OsirisEvent<*>>.messages: List<ChatMessage>
+public val List<OsirisEvent>.messages: List<ChatMessage>
   get() = filterIsInstance<OsirisEvent.Message>().map { it.message }.toList()
