b71b83a51c97bb5dc40fc89a2129ad535279caeb
Jeff Hudson <jeff@highbeam.co>
2025-06-02 12:01:12 -0600

    Helpers

--- osiris-testing/src/main/kotlin/osiris/testing/OsirisEvent.kt
+++ osiris-testing/src/main/kotlin/osiris/testing/OsirisEvent.kt
@@ -1,11 +1,10 @@
 package osiris.testing
 
 import dev.langchain4j.data.message.ChatMessage
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.filterIsInstance
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.toList
 import osiris.core.OsirisEvent
 
-public suspend fun Flow<OsirisEvent>.getMessages(): List<ChatMessage> =
-  filterIsInstance<OsirisEvent.Message>().map { it.message }.toList()
+public val <Response : Any> List<OsirisEvent<Response>>.response: Response?
+  get() = filterIsInstance<OsirisEvent.Response<Response>>().single().response
+
+public val List<OsirisEvent<*>>.messages: List<ChatMessage>
+  get() = filterIsInstance<OsirisEvent.Message>().map { it.message }.toList()
