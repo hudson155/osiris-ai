77f2fa3b108ff2dc1871f609cfbaa6d269cee218
Jeff Hudson <jeff@highbeam.co>
2025-06-03 22:49:41 -0600

    Entrypoint

--- osiris-agentic/src/main/kotlin/osiris/agentic/Network.kt
+++ osiris-agentic/src/main/kotlin/osiris/agentic/Network.kt
@@ -1,14 +1,17 @@
 package osiris.agentic
 
 public class Network(
+  public val entrypoint: String?,
   public val agents: Map<String, Agent>,
 )
 
 public class NetworkBuilder internal constructor() {
+  public var entrypoint: String? = null
   public val agents: MutableList<Agent> = mutableListOf()
 
   internal fun build(): Network =
     Network(
+      entrypoint = entrypoint,
       agents = agents.associateBy { it.name },
     )
 }
