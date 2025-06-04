b97bbae5b331d32a056c1420a2db3c32a6dfdff0
Jeff Hudson <jeff@highbeam.co>
2025-06-03 22:29:25 -0600

    Create agentic

---
+++ osiris-agentic/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+  id("osiris-ai")
+  id("osiris-ai-publish")
+}
+
+dependencies {
+  api(project(":osiris-core"))
+
+  implementation(libs.kairoLogging)
+
+  testImplementation(project(":osiris-evaluator"))
+  testImplementation(project(":osiris-open-ai"))
+
+  testImplementation(libs.kairoEnvironmentVariableSupplier)
+  testImplementation(libs.kairoLoggingTesting)
+  testImplementation(libs.kairoTesting)
+}
