ac40420c4a81995f2135c058a932092bb73524b2
Jeff Hudson <jeff@highbeam.co>
2025-06-06 17:57:54 -0600

    Fix broken instructions

--- osiris-agentic/src/main/kotlin/osiris/agentic/Instructions.kt
+++ osiris-agentic/src/main/kotlin/osiris/agentic/Instructions.kt
@@ -6,10 +6,14 @@ public abstract class Instructions(
   protected abstract val instructions: List<String>
 
   protected open fun combine(instructions: List<String>): String =
-    buildList {
-      if (includeDefaultInstructions) {
-        add(
-          """
+    instructions.joinToString(separator = "\n\n")
+
+  public fun create(instructions: String): String =
+    combine(
+      buildList {
+        if (includeDefaultInstructions) {
+          add(
+            """
             # The system
 
             You're a part of a multi-agent system.
@@ -17,11 +21,10 @@ public abstract class Instructions(
             When consulting other agents, succinctly tell them what to do or what you need.
             Don't tell them how to do their job.
           """.trimIndent(),
-        )
+          )
+        }
+        addAll(this@Instructions.instructions)
+        add(instructions)
       }
-      addAll(instructions)
-    }.joinToString(separator = "\n\n")
-
-  public fun create(instructions: String): String =
-    combine(this.instructions + instructions)
+    )
 }
