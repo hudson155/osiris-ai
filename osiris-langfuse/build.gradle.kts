ab69d537f819531e137919fd47ec0a8f97031536
Jeff Hudson <jeff@highbeam.co>
2025-06-09 13:19:30 -0600

    Connect to Langfuse for prompt management

---
+++ osiris-langfuse/build.gradle.kts
@@ -0,0 +1,9 @@
+plugins {
+  id("osiris-ai")
+  id("osiris-ai-publish")
+}
+
+dependencies {
+  api(libs.kairoProtectedString)
+  api(libs.kairoRestClient)
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,8 @@ rootProject.name = "osiris-ai"
 include(":osiris-agentic")
 include(":osiris-core")
 include(":osiris-evaluator")
+include(":osiris-langfuse")
+include(":osiris-langfuse:prompt")
 include(":osiris-open-ai")
 include(":osiris-schema")
 include(":osiris-testing")
