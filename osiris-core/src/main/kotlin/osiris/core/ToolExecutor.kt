1dae353a2263075548b4a34895583ba790d21d85
Jeff Hudson <jeff@highbeam.co>
2025-06-09 20:00:10 -0600

    Emit messages for tool calls

--- osiris-core/src/main/kotlin/osiris/core/ToolExecutor.kt
+++ osiris-core/src/main/kotlin/osiris/core/ToolExecutor.kt
@@ -2,16 +2,21 @@ package osiris.core
 
 import dev.langchain4j.agent.tool.ToolExecutionRequest
 import dev.langchain4j.data.message.ToolExecutionResultMessage
+import kotlinx.coroutines.flow.FlowCollector
+import osiris.event.Event
+import osiris.event.ToolEvent
 
 public abstract class ToolExecutor {
   public abstract suspend fun execute(
     tools: List<Tool<*, *>>,
+    flowCollector: FlowCollector<Event>,
     executionRequests: List<ToolExecutionRequest>,
   ): List<ToolExecutionResultMessage>
 
   public class Default : ToolExecutor() {
     override suspend fun execute(
       tools: List<Tool<*, *>>,
+      flowCollector: FlowCollector<Event>,
       executionRequests: List<ToolExecutionRequest>,
     ): List<ToolExecutionResultMessage> =
       executionRequests.map { executionRequest ->
@@ -19,7 +24,9 @@ public abstract class ToolExecutor {
         val toolName = executionRequest.name()
         val input = executionRequest.arguments()
         val tool = requireNotNull(tools.singleNullOrThrow { it.name == toolName }) { "No tool with name: $toolName." }
+        flowCollector.emit(ToolEvent.Start(tool = tool, id = id, input = input))
         val output = tool.execute(input)
+        flowCollector.emit(ToolEvent.End(tool = tool, id = id, input = input, output = output))
         return@map ToolExecutionResultMessage(id, toolName, output)
       }
   }
