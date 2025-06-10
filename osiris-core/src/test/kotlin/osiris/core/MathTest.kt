2ec2ead10460e39b2a7ea2e29ab4f3213aaad791
Jeff Hudson <jeff@highbeam.co>
2025-06-10 17:24:36 -0600

    lifecycle

--- osiris-core/src/test/kotlin/osiris/core/MathTest.kt
+++ osiris-core/src/test/kotlin/osiris/core/MathTest.kt
@@ -8,12 +8,10 @@ import io.kotest.matchers.shouldBe
 import io.kotest.matchers.types.shouldBeInstanceOf
 import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.TestInstance
 import osiris.openAi.openAi
 import osiris.span.ChatEvent
 import osiris.span.Span
 
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 internal class MathTest {
   private val messages: List<ChatMessage> = listOf(
     UserMessage("What's 2+2?"),
