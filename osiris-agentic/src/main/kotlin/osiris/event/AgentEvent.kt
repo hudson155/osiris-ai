1f16d5d4165551e0b07799a61dbf522ba61215f3
Jeff Hudson <jeff@highbeam.co>
2025-06-10 11:30:15 -0600

    Recursive Langfuse traces

--- osiris-agentic/src/main/kotlin/osiris/event/AgentEvent.kt
+++ osiris-agentic/src/main/kotlin/osiris/event/AgentEvent.kt
@@ -1,13 +1,28 @@
 package osiris.event
 
+import java.time.Instant
 import osiris.agentic.Agent
 
 public sealed class AgentEvent : Event() {
   public data class Start(
+    val at: Instant,
     val agent: Agent,
-  ) : AgentEvent()
+  ) : AgentEvent() {
+    @Suppress("ForbiddenMethodCall")
+    public constructor(agent: Agent) : this(
+      at = Instant.now(),
+      agent = agent,
+    )
+  }
 
   public data class End(
+    val at: Instant,
     val agent: Agent,
-  ) : AgentEvent()
+  ) : AgentEvent() {
+    @Suppress("ForbiddenMethodCall")
+    public constructor(agent: Agent) : this(
+      at = Instant.now(),
+      agent = agent,
+    )
+  }
 }
