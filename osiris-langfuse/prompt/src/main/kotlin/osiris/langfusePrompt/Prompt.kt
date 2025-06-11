5040a0a2abf7c05dc8e39245a1edee43156a04a0
Jeff Hudson <jeff@highbeam.co>
2025-06-11 07:37:12 -0600

    Introduce prompt compilation

--- osiris-langfuse/prompt/src/main/kotlin/osiris/langfusePrompt/Prompt.kt
+++ osiris-langfuse/prompt/src/main/kotlin/osiris/langfusePrompt/Prompt.kt
@@ -7,3 +7,27 @@ public data class Prompt(
   val name: String,
   val prompt: String,
 )
+
+public fun Prompt.compile(vararg variables: Pair<String, String>): String =
+  compile(strict = true, *variables)
+
+public fun Prompt.compile(
+  strict: Boolean,
+  vararg variables: Pair<String, String>,
+): String {
+  val map = variables.associate { it }
+  val regex = Regex("[{][{](?<key>[a-z][A-Za-z0-9]*)[}][}]")
+  val replacements = regex.findAll(prompt).associate { matchResult ->
+    val key = matchResult.groups["key"]!!.value
+    return@associate Pair(key, map[key])
+  }
+  return prompt.replace(regex) { match ->
+    val key = match.groups["key"]!!.value
+    val replacement = replacements[key]
+    if (replacement == null) {
+      if (strict) error("Unrecognized key: $key.")
+      return@replace match.value
+    }
+    return@replace replacement
+  }
+}
