c1f6f2b076fe9ccca11068da7690a4353335a1d0
Jeff Hudson <jeff@highbeam.co>
2025-06-13 12:32:08 -0600

    Remove util

--- osiris-core/src/main/kotlin/osiris/event/MessageEvent.kt
+++ osiris-core/src/main/kotlin/osiris/event/MessageEvent.kt
@@ -1,13 +1,7 @@
 package osiris.event
 
 import dev.langchain4j.data.message.ChatMessage
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.filterIsInstance
-import kotlinx.coroutines.flow.map
 
 public data class MessageEvent(
   val message: ChatMessage,
 ) : Event()
-
-public val Flow<Event>.messages
-  get() = filterIsInstance<MessageEvent>().map { it.message }
