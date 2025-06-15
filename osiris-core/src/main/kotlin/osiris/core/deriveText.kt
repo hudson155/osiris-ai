ac6e055ad58c7b53c46e87b43384a5e00f911eac
Jeff Hudson <jeff@highbeam.co>
2025-06-15 14:41:05 -0600

    Change derive text

--- osiris-core/src/main/kotlin/osiris/core/deriveText.kt
+++ osiris-core/src/main/kotlin/osiris/core/deriveText.kt
@@ -4,18 +4,11 @@ import dev.langchain4j.data.message.AiMessage
 import dev.langchain4j.data.message.ChatMessage
 import dev.langchain4j.data.message.UserMessage
 
-internal fun deriveText(messages: List<ChatMessage>): String =
-  messages.asReversed().firstNotNullOf { deriveText(it) }
-
-internal fun deriveText(message: ChatMessage): String? =
-  when (message) {
-    is AiMessage -> deriveText(message)
-    is UserMessage -> deriveText(message)
-    else -> null
+public fun deriveText(messages: List<ChatMessage>): String =
+  messages.asReversed().firstNotNullOf { message ->
+    when (message) {
+      is AiMessage -> message.text()
+      is UserMessage -> message.singleText()
+      else -> null
+    }
   }
-
-internal fun deriveText(message: AiMessage): String =
-  message.text()
-
-internal fun deriveText(message: UserMessage): String =
-  message.singleText()
