cf479cc89e5654f7646db845e9725c200b588a76
Jeff Hudson <jeff@highbeam.co>
2025-06-18 17:46:04 -0600

    More

--- osiris-prompt/src/test/kotlin/osiris/prompt/InstructionsTest.kt
+++ osiris-prompt/src/test/kotlin/osiris/prompt/InstructionsTest.kt
@@ -42,10 +42,11 @@ internal class InstructionsTest {
       """
         # The system
         
-        You're a part of a multi-agent system.
-        You can consult other agents.
-        When consulting other agents, succinctly tell them what to do or what you need.
-        Don't tell them how to do their job.
+        You're a part of a multi-agent system. You can consult other agents.
+        
+        ## Consulting other agents
+        
+        When consulting other agents, succinctly tell them what to do or what you need. Don't tell them how to do their job. Avoid rephrasing the user's question too much, since other agents might have more context to interpret it better than you can.
         
         The real instructions.
       """.trimIndent(),
