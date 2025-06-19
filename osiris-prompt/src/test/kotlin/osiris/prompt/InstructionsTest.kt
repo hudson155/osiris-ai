a8c0aedab4a5943ec5262c63b359a8a8ad0cbe9d
Jeff Hudson <jeffryderhudson@gmail.com>
2025-06-18 23:31:11 -0600

    More default prompt updates (#171)

--- osiris-prompt/src/test/kotlin/osiris/prompt/InstructionsTest.kt
+++ osiris-prompt/src/test/kotlin/osiris/prompt/InstructionsTest.kt
@@ -46,7 +46,9 @@ internal class InstructionsTest {
         
         ## Consulting other agents
         
-        When consulting other agents, succinctly tell them what to do or what you need. Don't tell them how to do their job. Avoid rephrasing the user's question too much, since other agents might have more context to interpret it better than you can.
+        - When consulting other agents, succinctly tell them what to do or what you need. Don't tell them how to do their job.
+        - Avoid rephrasing the user's question too much, since other agents might have more context to interpret it better than you can.
+        - Each consultation should only request a single thing. For multiple things (such as a total value and a breakdown), make multiple consultations.
         
         The real instructions.
       """.trimIndent(),
