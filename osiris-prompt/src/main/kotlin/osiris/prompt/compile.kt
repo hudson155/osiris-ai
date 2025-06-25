63e85d3716c78ef1585b0e6f6dd21ef9565798ed
Jeff Hudson <jeff@highbeam.co>
2025-06-24 22:04:36 -0600

    Allow async instruction compilation

--- osiris-prompt/src/main/kotlin/osiris/prompt/compile.kt
+++ osiris-prompt/src/main/kotlin/osiris/prompt/compile.kt
@@ -8,7 +8,7 @@ package osiris.prompt
  * Strict mode is used by default,
  * which throws an exception if a variable doesn't have a matching value in [buildMap].
  */
-public fun Instructions.compile(buildMap: MutableMap<String, String>.() -> Unit = {}): Instructions =
+public fun Instructions.compile(buildMap: suspend MutableMap<String, String>.() -> Unit = {}): Instructions =
   compile(strict = true, buildMap)
 
 /**
@@ -16,12 +16,12 @@ public fun Instructions.compile(buildMap: MutableMap<String, String>.() -> Unit 
  */
 public fun Instructions.compile(
   strict: Boolean,
-  buildMap: MutableMap<String, String>.() -> Unit = {},
+  buildMap: suspend MutableMap<String, String>.() -> Unit = {},
 ): Instructions {
   val delegate = this // This looks like 2010s JavaScript.
   return Instructions {
     val instructions = delegate.get()
-    val map = buildMap(buildMap)
+    val map = buildMap { buildMap() }
     val regex = Regex("[{][{] *(?<key>[A-Za-z0-9-_]*) *[}][}]")
     val replacements = regex.findAll(instructions).associate { matchResult ->
       val key = matchResult.groups["key"]!!.value
