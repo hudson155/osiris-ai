1279a9cf83898b7aca43dced00bdfd46309b59d3
Jeff Hudson <jeff@highbeam.co>
2025-06-26 12:19:10 -0600

    Pass only name

--- osiris-agentic/src/main/kotlin/osiris/agentic/Guardrail.kt
+++ osiris-agentic/src/main/kotlin/osiris/agentic/Guardrail.kt
@@ -3,11 +3,12 @@ package osiris.agentic
 import dev.langchain4j.data.message.ChatMessage
 
 public class Guardrail(
-  private val agent: Agent,
+  private val agentName: String,
   private val validate: (List<ChatMessage>) -> Unit,
 ) {
   public suspend fun execute() {
     val executionContext = getExecutionContext()
+    val agent = executionContext.getAgent(agentName)
     agent.execute()
     validate(executionContext.response)
   }
