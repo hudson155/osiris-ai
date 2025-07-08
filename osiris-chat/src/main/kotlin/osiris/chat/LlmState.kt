67d0575c66d2344b3000ae8a64d3bc4ca9b78b9b
Jeff Hudson <jeffryderhudson@gmail.com>
2025-07-08 17:54:25 -0600

    Keep track of chat request count (#228)

--- osiris-chat/src/main/kotlin/osiris/chat/LlmState.kt
+++ osiris-chat/src/main/kotlin/osiris/chat/LlmState.kt
@@ -3,5 +3,11 @@ package osiris.chat
 import dev.langchain4j.data.message.ChatMessage
 
 public data class LlmState(
+  val chatRequestCount: Int,
   val response: List<ChatMessage>,
-)
+) {
+  public constructor() : this(
+    chatRequestCount = 0,
+    response = emptyList(),
+  )
+}
