3ebaf2da1de677cecda9c08ff36aeaee26085e79
Jeff Hudson <jeff@highbeam.co>
2025-07-14 19:33:44 -0600

    Create helper for event transforms

--- osiris-langfuse/tracing/src/main/kotlin/osiris/langfuseTracing/TraceCreate.kt
+++ osiris-langfuse/tracing/src/main/kotlin/osiris/langfuseTracing/TraceCreate.kt
@@ -4,12 +4,12 @@ import java.time.Instant
 import kotlin.uuid.Uuid
 import osiris.langfuseTracing.TraceCreate.Body
 
-internal data class TraceCreate(
+public data class TraceCreate(
   override val id: Uuid,
   override val timestamp: Instant,
   override val body: Body,
 ) : IngestionEvent<Body>() {
-  internal data class Body(
+  public data class Body(
     override val id: Uuid,
     val timestamp: Instant,
     val name: String,
