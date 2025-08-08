9a61d8dad76d6d6a16433878b39f6d7cccd7ffd8
Jeff Hudson <jeff@highbeam.co>
2025-08-08 11:58:27 -0600

    Remove some stuff

--- osiris-langfuse/tracing/src/main/kotlin/osiris/langfuseTracing/IngestionEvent.kt
+++ osiris-langfuse/tracing/src/main/kotlin/osiris/langfuseTracing/IngestionEvent.kt
@@ -1,29 +1,11 @@
 package osiris.langfuseTracing
 
-import com.fasterxml.jackson.annotation.JsonAnyGetter
-import com.fasterxml.jackson.annotation.JsonAnySetter
 import java.time.Instant
 import kotlin.uuid.Uuid
-import osiris.tracing.TraceLevel
 
 public data class IngestionEvent(
   val id: Uuid,
   val type: String,
   val timestamp: Instant,
   val body: Map<String, Any?>,
-) {
-  public data class Body(
-    val id: Uuid,
-    val traceId: Uuid,
-    val parentObservationId: Uuid?,
-    val startTime: Instant,
-    val endTime: Instant,
-    val name: String,
-    val input: Any?,
-    val output: Any?,
-    val level: TraceLevel,
-    @field:JsonAnyGetter
-    @param:JsonAnySetter
-    val properties: Map<String, Any?> = emptyMap(),
-  )
-}
+)
