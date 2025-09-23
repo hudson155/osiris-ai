869cb535fb2c9a4847f44b975786f6a867451450
Jeff Hudson <jeff@highbeam.co>
2025-09-23 12:06:31 -0600

    Docs

---
+++ bom/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+  id("kairo-ai-platform")
+  id("kairo-ai-platform-publish")
+}
+
+dependencies {
+  /**
+   * Automatically include all other modules.
+   */
+  constraints {
+    rootProject.subprojects.forEach { subproject ->
+      if (subproject.name == project.name) return@forEach
+      evaluationDependsOn(subproject.path)
+      if (!subproject.plugins.hasPlugin("maven-publish")) return@forEach
+      subproject.publishing.publications.withType<MavenPublication> {
+        api("$groupId:$artifactId:$version")
+      }
+    }
+  }
+
+  // kairo
+  // https://github.com/hudson155/kairo/releases
+  api(platform("software.airborne.kairo:bom-full:LOCAL-SNAPSHOT"))
+
+  // langchain4j
+  // https://github.com/langchain4j/langchain4j/releases
+  api(platform("dev.langchain4j:langchain4j-bom:1.5.0"))
+}
