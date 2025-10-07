d0f7dc74d28721f09fb7449f6188a2957d374bd7
Jeff Hudson <jeff@highbeam.co>
2025-10-07 05:32:59 -0600

    Improve metadata interface (#308)

--- osiris-agent/src/main/kotlin/osiris/agent/metadata/NoopMetadata.kt
+++ osiris-agent/src/main/kotlin/osiris/agent/metadata/NoopMetadata.kt
@@ -4,6 +4,9 @@ public class NoopMetadata : Metadata() {
   override suspend fun get(): Map<String, String> =
     emptyMap()
 
-  override suspend fun set(key: String, value: String): Unit =
+  override suspend fun set(key: String, value: String?): Unit =
+    Unit
+
+  override suspend fun transform(key: String, transform: (value: String?) -> String?): Unit =
     Unit
 }
