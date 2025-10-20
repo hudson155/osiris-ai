547182e6c1c2ec3d94925e8953697cd03324395a
Jeff Hudson <jeffryderhudson@gmail.com>
2025-10-20 13:39:55 -0400

    Add schema support for sealed classes (#310)

--- osiris-schema/src/main/kotlin/osiris/schema/SchemaGenerator.kt
+++ osiris-schema/src/main/kotlin/osiris/schema/SchemaGenerator.kt
@@ -2,15 +2,18 @@ package osiris.schema
 
 import dev.langchain4j.model.chat.request.json.JsonAnyOfSchema
 import dev.langchain4j.model.chat.request.json.JsonBooleanSchema
+import dev.langchain4j.model.chat.request.json.JsonEnumSchema
 import dev.langchain4j.model.chat.request.json.JsonIntegerSchema
 import dev.langchain4j.model.chat.request.json.JsonNullSchema
 import dev.langchain4j.model.chat.request.json.JsonNumberSchema
 import dev.langchain4j.model.chat.request.json.JsonObjectSchema
 import dev.langchain4j.model.chat.request.json.JsonSchemaElement
 import dev.langchain4j.model.chat.request.json.JsonStringSchema
+import kotlinx.serialization.descriptors.PolymorphicKind
 import kotlinx.serialization.descriptors.PrimitiveKind
 import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.descriptors.StructureKind
+import kotlinx.serialization.descriptors.elementDescriptors
 import kotlinx.serialization.serializer
 
 public object SchemaGenerator {
@@ -22,6 +25,7 @@ public object SchemaGenerator {
     when (descriptor.kind) {
       is StructureKind.CLASS -> generateClass(descriptor, annotations)
       is StructureKind.OBJECT -> generateObject(descriptor, annotations)
+      is PolymorphicKind.SEALED -> generatePolymorphic(descriptor, annotations)
       is PrimitiveKind.BOOLEAN -> generateBoolean(descriptor, annotations)
       is PrimitiveKind.INT -> generateInteger(descriptor, annotations)
       is PrimitiveKind.LONG -> generateInteger(descriptor, annotations)
@@ -30,7 +34,6 @@ public object SchemaGenerator {
       is PrimitiveKind.STRING -> generateString(descriptor, annotations)
       else -> error("Unsupported kind (kind=${descriptor.kind}).")
     }
-  // TODO: Sealed class
   // TODO: Enum
   // TODO: List
 
@@ -47,22 +50,71 @@ public object SchemaGenerator {
         )
       }
     }
-    val schema = JsonObjectSchema.builder().apply {
-      annotation<Schema.Description>(allAnnotations)?.let { description(it.value) }
-      addProperties(properties)
-      required(properties.keys.toList())
-    }.build()
+    val schema = jsonObjectSchema(
+      description = annotation<Schema.Description>(allAnnotations)?.value,
+      properties = properties,
+    )
     return if (descriptor.isNullable) schema.orNull() else schema
   }
 
   private fun generateObject(descriptor: SerialDescriptor, annotations: List<Annotation>): JsonSchemaElement {
     val allAnnotations = descriptor.annotations + annotations
-    val schema = JsonObjectSchema.builder().apply {
+    val schema = jsonObjectSchema(
+      description = annotation<Schema.Description>(allAnnotations)?.value,
+      properties = emptyMap(),
+    )
+    return if (descriptor.isNullable) schema.orNull() else schema
+  }
+
+  private fun generatePolymorphic(descriptor: SerialDescriptor, annotations: List<Annotation>): JsonSchemaElement {
+    check(descriptor.elementsCount == 2) {
+      "Polymorphic descriptor must have exactly 2 elements (descriptor=$descriptor)."
+    }
+    val allAnnotations = descriptor.annotations + annotations
+    val optionsDescriptor = descriptor.getElementDescriptor(1)
+    val schema = JsonAnyOfSchema.builder().apply {
       annotation<Schema.Description>(allAnnotations)?.let { description(it.value) }
-      required()
+      anyOf(
+        buildList {
+          optionsDescriptor.elementDescriptors.forEach { optionDescriptor ->
+            add(
+              jsonObjectSchema(
+                description = annotation<Schema.Description>(optionDescriptor.annotations)?.value,
+                properties = buildMap {
+                  put(
+                    key = descriptor.getElementName(0),
+                    value = JsonEnumSchema.builder().apply {
+                      enumValues(optionDescriptor.serialName)
+                    }.build(),
+                  )
+                  repeat(optionDescriptor.elementsCount) { i ->
+                    put(
+                      key = optionDescriptor.getElementName(i),
+                      value = generate(
+                        descriptor = optionDescriptor.getElementDescriptor(i),
+                        annotations = optionDescriptor.getElementAnnotations(i),
+                      ),
+                    )
+                  }
+                },
+              ),
+            )
+          }
+        },
+      )
     }.build()
     return if (descriptor.isNullable) schema.orNull() else schema
   }
+
+  private fun jsonObjectSchema(
+    description: String?,
+    properties: Map<String, JsonSchemaElement>,
+  ): JsonObjectSchema =
+    JsonObjectSchema.builder().apply {
+      description?.let { description(it) }
+      addProperties(properties)
+      required(properties.keys.toList())
+    }.build()
 
   private fun generateBoolean(descriptor: SerialDescriptor, annotations: List<Annotation>): JsonSchemaElement {
     val schema = JsonBooleanSchema.builder().apply {
