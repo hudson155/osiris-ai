9a3fe149609f597210e46f4cadbaef844aa3aecc
Jeff Hudson <jeff@highbeam.co>
2025-11-20 14:06:32 -0600

    Lint more

--- osiris-element/src/main/kotlin/osiris/element/parser/BlockQuoteMarkdownParser.kt
+++ osiris-element/src/main/kotlin/osiris/element/parser/BlockQuoteMarkdownParser.kt
@@ -1,8 +1,8 @@
 package osiris.element.parser
 
 import com.vladsch.flexmark.ast.BlockQuote
-import osiris.element.element.Element
 import osiris.element.element.BlockQuoteElement
+import osiris.element.element.Element
 
 internal object BlockQuoteMarkdownParser : MarkdownParser<BlockQuote>() {
   override fun parse(node: BlockQuote): List<Element> {
