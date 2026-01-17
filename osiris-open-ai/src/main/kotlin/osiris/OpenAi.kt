08694df7b1be1d0751bdb9d2d68fef7b3566041c
Jeff Hudson <jeff@highbeam.co>
2026-01-17 10:08:39 -0700

    OpenAI

---
+++ osiris-open-ai/src/main/kotlin/osiris/OpenAi.kt
@@ -0,0 +1,31 @@
+package osiris
+
+import dev.langchain4j.model.openai.OpenAiChatModel
+import dev.langchain4j.model.openai.OpenAiChatModel.OpenAiChatModelBuilder
+import io.ktor.util.AttributeKey
+import kairo.protectedString.ProtectedString
+
+private val key: AttributeKey<ProtectedString> = AttributeKey("openAiApiKey")
+
+public var ModelFactory.openAiApiKey: ProtectedString?
+  get() = attributes.getOrNull(key)
+  set(value) {
+    if (value != null) {
+      attributes[key] = value
+    } else {
+      attributes.remove(key)
+    }
+  }
+
+public fun ModelFactory.openAi(name: String, block: OpenAiChatModelBuilder.() -> Unit = {}): Model {
+  val apiKey = requireNotNull(openAiApiKey) { "OpenAI API key must be set to create a model." }
+  val model = OpenAiChatModel.builder().apply {
+    modelName(name)
+    @OptIn(ProtectedString.Access::class)
+    apiKey(apiKey.value)
+    strictJsonSchema(true)
+    strictTools(true)
+    block()
+  }.build()
+  return Model(model)
+}
