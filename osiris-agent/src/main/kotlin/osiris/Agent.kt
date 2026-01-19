24bf79e976c6fd780592b6f3bb0be7ec8bf4d0dc
Jeff Hudson <jeff@highbeam.co>
2026-01-19 05:56:20 -0700

    Keep track of current agent

--- osiris-agent/src/main/kotlin/osiris/Agent.kt
+++ osiris-agent/src/main/kotlin/osiris/Agent.kt
@@ -1,5 +1,7 @@
 package osiris
 
+import io.ktor.util.AttributeKey
+
 /**
  * The Osiris [Agent] interface can be used to implement any type of agent.
  * It's not strictly tied to being an LLM implementation.
@@ -8,5 +10,24 @@ package osiris
  */
 public abstract class Agent(public val name: String) {
   context(context: Context)
-  public abstract suspend fun execute()
+  public suspend fun execute() {
+    context.with(Context::currentAgent, this) {
+      executeAgent()
+    }
+  }
+
+  context(context: Context)
+  protected abstract suspend fun executeAgent()
 }
+
+private val key: AttributeKey<Agent> = AttributeKey("currentAgent")
+
+public var Context.currentAgent: Agent?
+  get() = attributes.getOrNull(key)
+  set(value) {
+    if (value != null) {
+      attributes[key] = value
+    } else {
+      attributes.remove(key)
+    }
+  }
