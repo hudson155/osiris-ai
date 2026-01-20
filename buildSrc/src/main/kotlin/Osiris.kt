56005caa5cac2bc4fc952bbaf85fcae1085d3942
Jeff Hudson <jeff@highbeam.co>
2026-01-20 16:24:09 -0700

    Real version

--- buildSrc/src/main/kotlin/Osiris.kt
+++ buildSrc/src/main/kotlin/Osiris.kt
@@ -15,7 +15,7 @@ internal fun RepositoryHandler.artifactRegistry() {
 
 public object Airborne {
   // https://github.com/hudson155/kairo/releases
-  public const val kairo: String = "software.airborne.kairo:bom-full:9999-SNAPSHOT"
+  public const val kairo: String = "software.airborne.kairo:bom-full:6.0.0-beta.15"
 }
 
 internal fun groupId(): String =
