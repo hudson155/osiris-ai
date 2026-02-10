5d5e0461726f453192acd9986939f02b8a4d7da2
Jeff Hudson <jeff@highbeam.co>
2026-02-10 10:09:53 -0500

    Add Gemini support

---
+++ osiris-gemini/build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+  id("osiris-library")
+  id("osiris-library-publish")
+}
+
+dependencies {
+  compileOnly(project(":osiris-llm"))
+
+  api(libs.kairo.protectedString)
+  api(libs.langchain4j.googleAiGemini)
+}
